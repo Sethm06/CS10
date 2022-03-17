aa73430ee86a41f6d4179b30cc793d99b8dd011e
16076006 <16076006@L815WD150037.edu.cbe.ab.ca>
2022-03-17 14:53:55 -0600

    Update 3

--- CS10/src/Assignments/myInitials.java
+++ CS10/src/Assignments/myInitials.java
@@ -17,13 +17,13 @@ public class myInitials
 
 	public static void main(String[] args) 
 	{
-		System.out.print("------  ||     ||\n");
-		System.out.println("|       | |   | |");
-		System.out.println("|       |  | |  |");
-		System.out.print("------  |   |   |\n");
-		System.out.println("     |  |       |");
-		System.out.println("     |  |       |");
-		System.out.print("------  |       |");
+		System.out.print("------    ||     ||\n");
+		System.out.println("|         | |   | |");
+		System.out.println("|         |  | |  |");
+		System.out.print("------    |   |   |\n");
+		System.out.println("     |    |       |");
+		System.out.println("     |    |       |");
+		System.out.print("------    |       |");
 		
 	}
 
