b7b0d58490508352e2c3ed1f3ccdb8c344ac1315
16076006 <16076006@L815WD150037.edu.cbe.ab.ca>
2022-03-17 15:08:06 -0600

    Update 4

---
+++ CS10/src/Assignments/BingoCard.java
@@ -0,0 +1,46 @@
+/*
+
+Program: BingoCard          Date: 17/03/2022
+
+
+Author: Seth Mckenzie 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+package Assignments;
+
+public class BingoCard 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("B\tI\tN\tG\tO\n");
+		System.out.println("1\t26\t36\t60\t68\n");
+		System.out.println("13\t19\t32\t58\t66\n");
+		System.out.println("4\t24     Free\t55\t69\n");
+		System.out.println("11\t28\t47\t48\t70\n");
+		System.out.println("6\t17\t40\t53\t63\n");
+		
+	}
+
+}
+
+/* Screen Dump
+
+B	I	N	G	O
+
+1	26	36	60	68
+
+13	19	32	58	66
+
+4	24     Free	55	69
+
+11	28	47	48	70
+
+6	17	40	53	63
+
+*/
