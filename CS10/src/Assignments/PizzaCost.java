bc1710b6666c78602e08f7a58edd6c572315f37c
16076006 <16076006@L815WD220013.edu.cbe.ab.ca>
2022-04-01 10:46:29 -0600

    Update - assignment 4

--- CS10/src/Assignments/PizzaCost.java
+++ CS10/src/Assignments/PizzaCost.java
@@ -1,6 +1,6 @@
 /*
 
-Program: PizzaCost          Date: 18/03/2022
+Program: PizzaCost          Date: 31/03/2022
 
 
 Author: Seth Mckenzie 
@@ -10,22 +10,34 @@ Course: Computer Science 10
 
 */
 
+
+
 package Assignments;
+
+
+import java.util.*;
 
 public class PizzaCost 
 {
 
-	public static void main(String[] args) 
-	{
-		
-		
-
-	}
-
+  public static void main(String[] args) 
+  	{
+	   
+	    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Enter the diameter of the pizza in inches:");
+	  	    
+	    double diameter = myObj.nextDouble();    
+	    
+	    System.out.println("The cost of making the pizza is: " + "$" + (diameter * 0.5 * 0.5 + 1 + 0.75));  // Output user input
+	    
+	 }
 }
-
 /* Screen Dump
 
-
+	
+Enter the diameter of the pizza in inches:
+1
+The cost of making the pizza is: $2.0
+	
 
 */
