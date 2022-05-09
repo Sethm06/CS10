1b0f6cd7d33f37e828373238e4db4da13b5337ce
16076006 <16076006@L815WD220013.edu.cbe.ab.ca>
2022-05-09 13:17:01 -0600

    Update

--- Credit5/src/Project/JavaFinalProject.java
+++ Credit5/src/Project/JavaFinalProject.java
@@ -1,6 +1,6 @@
 /*
 
-Program: JavaFinalProject       Date:05/05/2022
+Program: JavaFinalProject       Date:09/05/2022
 
 
 Author: Seth Mckenzie 
@@ -19,6 +19,7 @@ public class JavaFinalProject
   public static void main(String[] args) 
   {
 	 
+	//set up variables
 	String truePassword = null;
 	String write = null;
 	boolean inAccount = false; 
@@ -29,19 +30,22 @@ public class JavaFinalProject
 	System.out.println("Want to create a password? (y/n): ");
 	String YN1 = askYN1.next();
 	
+	//user makes a password
 	while(YN1.equals("y") || YN1.equals("Y"))
 	{
 			  //scanner create password
 			  Scanner askPassword = new Scanner(System.in);
-			  System.out.println("Create a numerical password with more than 4 digits: ");
+			  System.out.println("Create a password with more than 4 characters: ");
 			  String password = askPassword.next();
 			  
+			  //password not long enough
 			  while(password.length() < 4)
 			  {
-				  System.out.println("Please re-enter password with more than 4 digits: ");
+				  System.out.println("Please re-enter password with more than 4 characters: ");
 				  password = askPassword.next();
 			  }
 			  
+			  //password long enough
 			  while(password.length() >= 4)
 			  {
 				  //scanner confirm password
@@ -84,24 +88,28 @@ public class JavaFinalProject
 								System.out.println("");
 								inAccount = true;
 								
+								//user is in account
 								while(inAccount = true)
 								{
+									//what user can do in account
 									System.out.println("Here are some things you can do in your account:");
-									System.out.println("	- Addition\n	- Multiplacation\n	- Writing");
+									System.out.println("	- Addition\n	- Multiplication\n	- Writing");
 	
 									
 									//scanner account tasks
 									Scanner askTasks = new Scanner(System.in);
-									System.out.println("What do you want to do? Type + for addition, * for multiplacation, write for writing, and  log out for logging out!");
+									System.out.println("What do you want to do? Type + for addition, * for multiplication, write for writing, and exit for logging out!");
 									String tasks = askTasks.next();
 									inAccount = false;
 									
 									//addition
 									if (tasks.equals("+"))
 									{
+										//get random digits
 										int addDigitOne = (int)(Math.random() * 100);
 										int addDigitTwo = (int)(Math.random() * 100);
 										
+										//add digits
 										int addSolution = addDigitOne + addDigitTwo;
 										
 										//scanner ask solution?					
@@ -109,12 +117,15 @@ public class JavaFinalProject
 										System.out.println("What is the solution to " + addDigitOne + " + " + addDigitTwo + "?");
 										int addSolutionInpt  = askAddSolutionInpt.nextInt();
 										
+										//solution correct
 										if(addSolutionInpt == addSolution)
 										{
 											System.out.println("You got it right!");
 											System.out.println("");
 											inAccount = true;
 										}
+										
+										//solution wrong
 										else if(addSolutionInpt != addSolution)
 										{
 											System.out.println("You got it wrong!");
@@ -126,9 +137,11 @@ public class JavaFinalProject
 									//multiplication 
 									if (tasks.equals("*"))
 									{
+										//get random digits
 										int multiDigitOne = (int)(Math.random() * 20);
 										int multiDigitTwo = (int)(Math.random() * 20);
 										
+										//multiply digits
 										int multiProduct = multiDigitOne * multiDigitTwo;
 										
 										//scanner ask product?					
@@ -136,12 +149,15 @@ public class JavaFinalProject
 										System.out.println("What is the product to " + multiDigitOne + " * " + multiDigitTwo + "?");
 										int multiProductInpt  = askMultiProductInpt.nextInt();
 										
+										//product correct
 										if(multiProductInpt == multiProduct)
 										{
 											System.out.println("You got it right!");
 											System.out.println("");
 											inAccount = true;
 										}
+										
+										//product wrong
 										else if(multiProductInpt != multiProduct)
 										{
 											System.out.println("You got it wrong!");
@@ -154,18 +170,22 @@ public class JavaFinalProject
 									if(tasks.equals("write"))
 									{	
 										
+										//is there is previous writing?
 										if(write != null)
 										{
-											//scanner load last writing?					
+											//scanner load previous writing?					
 											Scanner askLoadWrite = new Scanner(System.in);
 											System.out.println("Would you like to load your last writing? (y/n)");
 											String loadWrite  = askLoadWrite.next();
 											
+											//load previous writing
 											if(loadWrite.equals("y") || loadWrite.equals("Y"))
 											{
 												System.out.println(write);
 												System.out.println("");
 											}
+											
+											//don't load previous writing
 											else if(loadWrite.equals("n") || loadWrite.equals("N"))
 											{
 												System.out.println("Ok.");
@@ -175,7 +195,7 @@ public class JavaFinalProject
 										//scanner write?					
 										Scanner askWrite = new Scanner(System.in);
 										System.out.println("Write what you want!");
-										write  = askWrite.next();
+										write  = askWrite.nextLine();
 										
 										System.out.println("Nice writing!");
 										
@@ -183,26 +203,30 @@ public class JavaFinalProject
 										Scanner askYN3 = new Scanner(System.in);
 										System.out.println("Would you like to save it? (y/n)");
 										String YN3  = askYN3.next();
-										
-										if(YN1.equals("y") || YN1.equals("Y"))
-										{
-											System.out.println("Saving...");
-											System.out.println("Writing saved!");
-											System.out.println("");
-											inAccount = true;
-										}
-										if(YN1.equals("n") || YN1.equals("N"))
-										{
-											System.out.println("Writing not saved.");
-											System.out.println("");
-											write = null;
-											inAccount = true;
-										}
+											
+											//save writing
+											if(YN3.equals("y") || YN3.equals("Y"))
+											{
+												System.out.println("Writing saved!");
+												System.out.println("");
+												inAccount = true;
+											}
+											
+											//don't save writing
+											else if(YN3.equals("n") || YN3.equals("N"))
+											{
+												System.out.println("Writing not saved.");
+												System.out.println("");
+												write = null;
+												inAccount = true;
+											}
+
 									}
 									
 									//log out
-									else if(tasks.equals("log out"))
+									if(tasks.equals("exit"))
 									{
+										System.out.println("Logging out...");
 										System.out.println("Thank you for using this program!");
 										System.exit(0);
 									}
@@ -212,6 +236,7 @@ public class JavaFinalProject
 								
 							}
 							
+							//wrong password
 							while(!typePassword.equals(truePassword))
 							{
 								System.out.println("Wrong password, access to profile denied.");
@@ -229,7 +254,9 @@ public class JavaFinalProject
 					}
 					  
 				  }
-				  if(!password.equals(passwordAgain))//passwords don't match
+				  
+				  //passwords don't match
+				  if(!password.equals(passwordAgain))
 				  {
 					  System.out.println("Wrong, passwords do not match.");
 					  System.out.println("Please re-enter password with more than 4 digits: ");
@@ -237,6 +264,8 @@ public class JavaFinalProject
 				  }
 			  }
 	}
+	
+	//user doesn't make password
 	if(YN1.equals("n") || YN1.equals("N"))
 	{
 		System.out.println("Thank you for using this program!");
@@ -255,7 +284,83 @@ public class JavaFinalProject
 /* Screen Dump
 
 
+Want to create a password? (y/n): 
+y
+Create a password with more than 4 characters: 
+123
+Please re-enter password with more than 4 characters: 
+1234
+Type in your password again to confirm: 
+1234
+Passwords match, saving password.
+Type in your name: 
+Seth McKenzie
+Want to access your profile? (y/n): 
+y
+Type in you password: 
+1234
+Welcome to your profile!
 
+Your name is Seth
+
+Here are some things you can do in your account:
+	- Addition
+	- Multiplication
+	- Writing
+What do you want to do? Type + for addition, * for multiplication, write for writing, and exit for logging out!
++
+What is the solution to 42 + 7?
+49
+You got it right!
+
+Here are some things you can do in your account:
+	- Addition
+	- Multiplication
+	- Writing
+What do you want to do? Type + for addition, * for multiplication, write for writing, and exit for logging out!
+*
+What is the product to 2 * 5?
+10
+You got it right!
+
+Here are some things you can do in your account:
+	- Addition
+	- Multiplication
+	- Writing
+What do you want to do? Type + for addition, * for multiplication, write for writing, and exit for logging out!
+write
+Write what you want!
+Hello, my name is Seth and this is my java final project
+Nice writing!
+Would you like to save it? (y/n)
+y
+Writing saved!
+
+Here are some things you can do in your account:
+	- Addition
+	- Multiplication
+	- Writing
+What do you want to do? Type + for addition, * for multiplication, write for writing, and exit for logging out!
+write
+Would you like to load your last writing? (y/n)
+y
+Hello, my name is Seth and this is my java final project
+
+Write what you want!
+Blah blah blah
+Nice writing!
+Would you like to save it? (y/n)
+n
+Writing not saved.
+
+Here are some things you can do in your account:
+	- Addition
+	- Multiplication
+	- Writing
+What do you want to do? Type + for addition, * for multiplication, write for writing, and exit for logging out!
+exit
+Logging out...
+Thank you for using this program!
 
 
 */
